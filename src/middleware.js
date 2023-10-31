1148f1a4991e7d70cfdd355dcf698465f630433d
Bertrand Riviere <bertrand.riviere@gmail.com>
2023-10-31 13:56:58 +0100

    remove staticwebapp.config.json content for now and ensure middleware "works"

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,10 @@
 import { NextResponse } from 'next/server';
 
 export const middleware = async (req) => {
+  if (req.nextUrl.pathname.startsWith('/page-no')) {
+    return new NextResponse('Auth required', { status: 401 });
+  }
+
   if (req.nextUrl.pathname.startsWith('/page-c')) {
     return NextResponse.rewrite(new URL('/page-d', req.url));
   }
