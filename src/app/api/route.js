f001ba54eb0b82fd686145a86546a942952f1ec0
Bertrand Riviere <bertrand.riviere@gmail.com>
2023-11-03 14:54:24 +0100

    test axios

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,25 +1,45 @@
 import { NextResponse } from 'next/server';
 import crypto from 'crypto';
-import { Agent } from 'undici';
+import https from 'https';
+import axios from 'axios';
+//import { Agent } from 'undici';
 
-export const GET = async () => {
+export const GET = async (req) => {
   try {
-    const response = await fetch(
+    // const response = await fetch(
+    //   'https://stats.oecd.org/SDMX-JSON/data/NAAG/FRA.DBTS13GDP/?startTime=2006',
+    //   {
+    //     headers: {
+    //       Accept: 'application/vnd.sdmx.data+json',
+    //       'Accept-Language': 'en',
+    //     },
+    //     // dispatcher: new https.Agent({
+    //     //   connect: {
+    //     //     rejectUnauthorized: false,
+    //     //     secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+    //     //   },
+    //     // }),
+    //     // dispatcher: new Agent({
+    //     //   connect: {
+    //     //     rejectUnauthorized: false,
+    //     //     secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+    //     //   },
+    //     // }),
+    //   },
+    // );
+    // const dotStatData = await response.json();
+
+    const response = await axios.get(
       'https://stats.oecd.org/SDMX-JSON/data/NAAG/FRA.DBTS13GDP/?startTime=2006',
       {
-        headers: {
-          Accept: 'application/vnd.sdmx.data+json',
-          'Accept-Language': 'en',
-        },
-        dispatcher: new Agent({
-          connect: {
-            rejectUnauthorized: false,
-            secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
-          },
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+          secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
         }),
       },
     );
-    const dotStatData = await response.json();
+
+    const dotStatData = response.data;
 
     return NextResponse.json(dotStatData);
   } catch (e) {
