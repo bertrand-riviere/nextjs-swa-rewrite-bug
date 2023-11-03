e2dd1fd5d4634e94260ca042719a737ce2705754
Bertrand Riviere <bertrand.riviere@gmail.com>
2023-11-03 13:41:13 +0100

    log error

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,22 +3,26 @@ import crypto from 'crypto';
 import { Agent } from 'undici';
 
 export const GET = async () => {
-  const response = await fetch(
-    'https://stats.oecd.org/SDMX-JSON/data/NAAG/FRA.DBTS13GDP/?startTime=2006',
-    {
-      headers: {
-        Accept: 'application/vnd.sdmx.data+json',
-        'Accept-Language': 'en',
-      },
-      dispatcher: new Agent({
-        connect: {
-          rejectUnauthorized: false,
-          secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+  try {
+    const response = await fetch(
+      'https://stats.oecd.org/SDMX-JSON/data/NAAG/FRA.DBTS13GDP/?startTime=2006',
+      {
+        headers: {
+          Accept: 'application/vnd.sdmx.data+json',
+          'Accept-Language': 'en',
         },
-      }),
-    },
-  );
-  const dotStatData = await response.json();
+        dispatcher: new Agent({
+          connect: {
+            rejectUnauthorized: false,
+            secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+          },
+        }),
+      },
+    );
+    const dotStatData = await response.json();
 
-  return NextResponse.json(dotStatData);
+    return NextResponse.json(dotStatData);
+  } catch (e) {
+    return NextResponse.json({ error: e });
+  }
 };
