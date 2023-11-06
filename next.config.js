24201db28448b022925f73e7f341a0705f750843
Bertrand Riviere <bertrand.riviere@gmail.com>
2023-11-06 12:00:36 +0100

    remove previous test changes

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/edit/:id',
-        destination: '/edit?id=:id',
-      },
-      {
         source: '/page-a',
         destination: '/page-b',
       },
